f03df54d2ec42470e5ed1cf632e7a4dd38eeb82b
smithc57973 <44327733+smithc57973@users.noreply.github.com>
2020-09-08 16:31:37 -0500

    Created Collision and Despawning

---
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    public float topBound = 20;
+    public float bottomBound = -10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Food goes out of bounds
+        if(transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+
+        //Animal goes out of bounds
+        if (transform.position.z < bottomBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Prototype 2/Assets/Scripts/ShootPrefab.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootPrefab : MonoBehaviour
+{
+    public GameObject prefabToShoot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefabToShoot, transform.position, prefabToShoot.transform.rotation);
+        }
+    }
+}
